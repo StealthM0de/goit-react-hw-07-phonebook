b332cff5d84b94b284c9c9f73edff6175aadfbf3
StealthM0de <jedongfallorina@gmail.com>
2024-03-30 00:53:18 +0800

    contactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,27 +1,54 @@
-import { ContactListItem } from 'components/ContactList/ContactListItem/ContactListItem';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
-import { getContacts, getFilter } from '../../redux/selectors';
 
-const getFilteredContacts = (contacts, filter) => {
-  return contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-};
+import {
+  selectFilteredContacts,
+  selectError,
+  selectIsLoading,
+} from '../../redux/contacts/contactsSelector';
+import { fetchContacts } from '../../redux/contacts/contactsOperation';
+import { ContactListItem } from './ContactListItem/ContactListItem';
+import { Loader } from 'components/Loader/Loader';
+
+// const getFilteredContacts = (contacts, filter) => {
+//   return contacts.filter(contact =>
+//     contact.name.toLowerCase().includes(filter.toLowerCase())
+//   );
+// };
 
 export const ContactList = () => {
-  const contacts = useSelector(getContacts);
-  const filter = useSelector(getFilter);
+  const filteredContacts = useSelector(selectFilteredContacts);
+  const error = useSelector(selectError);
+  const isLoading = useSelector(selectIsLoading);
+  const dispatch = useDispatch();
+  // const contacts = useSelector(getContacts);
+  // const filter = useSelector(getFilter);
 
-  const filteredContacts = getFilteredContacts(contacts, filter);
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <ul>
-      {filteredContacts.map(filteredContact => (
-        <ContactListItem
-          key={filteredContact.id}
-          filteredContact={filteredContact}
-        />
-      ))}
+      {/* if loading and not error, show Loader */}
+      {isLoading && !error && <Loader />}
+
+      {/* if not loading, not error and filtered contacts is empty, show warning */}
+      {!isLoading && !error && filteredContacts.length === 0 && (
+        <p>The Phonebook is empty. Please add a contact</p>
+      )}
+
+      {/* if not loading, not error and have atleast 1 filtered contact, show ContactListItem */}
+      {!isLoading &&
+        !error &&
+        filteredContacts.length > 0 &&
+        filteredContacts.map(filteredContact => (
+          <ContactListItem
+            key={filteredContact.id}
+            filteredContact={filteredContact}
+          />
+        ))}
     </ul>
   );
 };
