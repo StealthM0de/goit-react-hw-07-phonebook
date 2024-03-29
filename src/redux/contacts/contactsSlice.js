86de163f10866eabc4040ab5a8c8242e77b49dfb
StealthM0de <jedongfallorina@gmail.com>
2024-03-30 00:05:27 +0800

    createAsyncThunk

--- src/redux/contacts/contactsSlice.js
+++ src/redux/contacts/contactsSlice.js
@@ -2,57 +2,12 @@ import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './contactsOperation';
 
 const contactsSlice = createSlice({
-  // namespace
   name: 'contacts',
   initialState: {
-    items: [
-      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    items: [],
     isLoading: false,
     error: null,
   },
-
-  //   reducers: {
-  //     addContact: {
-  //       reducer: (state, action) => {
-  //         state.initialContacts.push(action.payload);
-  //       },
-  //       // preprocesses the payload before dispatching the action
-  //       prepare: (name, number) => {
-  //         return {
-  //           payload: {
-  //             id: nanoid(),
-  //             name: name.trim(),
-  //             number: number.trim(),
-  //           },
-  //         };
-  //       },
-  //     },
-
-  //     deleteContact: (state, action) => {
-  //       const index = state.initialContacts.findIndex(
-  //         contact => contact.id === action.payload
-  //       );
-  //       state.initialContacts.splice(index, 1);
-  //     },
-  //   },
-  // });
-
-  // const persistConfig = {
-  //   key: 'contacts',
-  //   storage,
-  // };
-
-  // export const contactsReducer = persistReducer(
-  //   persistConfig,
-  //   contactsSlice.reducer
-  // );
-
-  // export const { addContact, deleteContact, deleteAllContacts } =
-  //   contactsSlice.actions;
 
   extraReducers: builder => {
     builder
